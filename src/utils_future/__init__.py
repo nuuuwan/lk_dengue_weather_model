5f21a9a5595db860d7884fae8bc8338789e02560
nuuuwan <nuuuwan@gmail.com>
2026-06-26 16:14:41 +0530

    Refactor ReadMe and RiskMap classes by removing unused code and optimizing imports in utils_future

--- src/utils_future/__init__.py
+++ src/utils_future/__init__.py
@@ -3,9 +3,7 @@
 
 from utils_future.console import (COLOR_BACKGROUND, COLOR_FOREGROUND,
                                   COLOR_FORMAT, LEVEL_TO_STYLE, Console, Log)
-from utils_future.file import (File, FileOrDirectory, JSONFile, PDFFile,
-                               PDFFileCompressMixin, PDFTextMixin, TSVFile,
-                               XSVFile)
+from utils_future.file import File, FileOrDirectory, JSONFile, TSVFile, XSVFile
 from utils_future.time import (Time, TimeDelta, TimeFormat, TimeUnit,
                                TimeZoneOffset)
 from utils_future.WWW import WWW
--- src/utils_future/file/__init__.py
+++ src/utils_future/file/__init__.py
@@ -4,8 +4,5 @@
 from utils_future.file.File import File
 from utils_future.file.FileOrDirectory import FileOrDirectory
 from utils_future.file.JSONFile import JSONFile
-from utils_future.file.pdf_file import (PDFFile, PDFFileCompressMixin,
-                                        PDFTextMixin)
-from utils_future.file.PDFFile import PDFFile
 from utils_future.file.TSVFile import TSVFile
 from utils_future.file.XSVFile import XSVFile
