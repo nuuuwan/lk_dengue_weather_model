938e10715cfc4819345bdac8eb92fff80cd8f8b2
nuuuwan <nuuuwan@gmail.com>
2026-06-26 16:21:22 +0530

    Refactor code structure for improved readability and maintainability

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -4,8 +4,7 @@ from moh import MOH, ReadMe, RiskMap
 def main():
     moh_list = MOH.list()
     for moh in moh_list:
-        if moh.district_id == "LK-11":
-            moh.build_all()
+        moh.build_all()
     RiskMap.build(moh_list)
     ReadMe.build(moh_list)
 
